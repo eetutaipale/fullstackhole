f1617d95ea02d9255c590cf94a1fea654d930086
unknown <eetaip@utu.fi>
2022-10-05 11:02:12 +0300

    1.4 ja 1.5 tehty

--- osa1/part1/src/App.js
+++ osa1/part1/src/App.js
@@ -16,55 +16,57 @@ const Part = (props) => {
   return (
     <div>
       <p>
-      {props.parts} {props.exercises}
+      {props.part.name} {props.part.exercises}
       </p>
     </div>
   )
 }
 const Total = (props) => {
-  
- return [
+
+ return (
   <>
   <p>
-    Number of exercises {props.parts(exercises[0])}
+    Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
   </p>
   </>
- ]
+ )
 }
 const Header = (props) => {
   
   return (
     <div>
     <h1>
-      {props.course} 
+      {props.course.name} 
     </h1> 
     </div>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
         <Header course={course} />
         
-        <Content parts={parts}/>
-        <Total parts={parts} />
+        <Content parts={course.parts}/>
+        <Total parts={course.parts} />
     </div>
   )
 }
