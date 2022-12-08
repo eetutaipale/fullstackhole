c34e38d4d19469f434cc935529505bd78b857598
unknown <eetaip@utu.fi>
2022-12-08 18:18:59 +0200

    2.2 tehtavat

--- osa2/part1/src/App.js
+++ osa2/part1/src/App.js
@@ -1,118 +1,33 @@
 
+import { useState } from 'react'
+
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 28,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
-  
-      return (
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={setPersons}>
         <div>
-          <Course course={courses} />
+          name: <input />
         </div>
-      )
-    }
-    const Course = (course) => {
-      
-  
-      const Content = (props) => {
-        return (
         <div>
-            {props.partes.map(par =>
-            <Part key ={par.id} part={par} />
-            )}
+          <button type="submit">add</button>
         </div>
-        )
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  )
 
-        }
-        const Part = (props) => {
-
-          return (
-              
-              <div>
-              <p>
-              {props.part.name} {props.part.exercises} 
-              </p>
-              </div>
-          )
-          }
-
-
-        const Total = (props) => {
-            const total = props.total.reduce( (s, p) => {return s + p.exercises;}, 0);
-
-              return (
-              <>
-              <h4>
-                  total of {total} exercises 
-              </h4>
-              </>
-              )
-        }
-
-
-        const Header = (props) => {
-
-              return (
-                  <div>
-                  <h2>
-                  {props.courses.name} 
-                  </h2> 
-                  </div>
-              )
-
-        }
-
-      return (
-          <><h1>
-              Web debelopment curriculum
-          </h1>
-          <div>
-          {course.course.map((cours, cours2, cours3) => <><Header key={cours.id} courses={cours} />
-              <Content key={cours2.id} partes={cours.parts} />
-              <Total key={cours3.id} total={cours.parts} />
-          </>)}
-          </div></>
-          )
-    }
+}
 
 export default App
