d27ff300e9f470f391fe9135cefd43c2f1c7e066
unknown <eetaip@utu.fi>
2023-05-16 10:05:40 +0300

    väli

--- osa3/index.js
+++ osa3/index.js
@@ -1,10 +1,36 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
-})
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+let persons = [
+    {
+        id: 1,
+        content: "Arto Hellas",
+        number: "123142"
+      },
+      {
+        id: 2,
+        content: "ada Lovelace",
+        number: "123123"
+      },
+      {
+        id: 3,
+        content: "Dan abramoc",
+        number: "2124152"
+      }
+]
+let info = {}
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+  app.get('/info', (req, res) => {
+
+    res.send(
+        `Phonebook has info for ${persons.length} people`
+        )
+  })
+  
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}/api/persons`)
+  })
